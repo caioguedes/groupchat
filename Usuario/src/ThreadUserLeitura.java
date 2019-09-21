c10a647ce0e35ffbf3232a9dff1f41bd6c047957
Lucas Schaan <schaanlucas@gmail.com>
2019-09-21 00:02:54 -0300

    Adicionada verificação para conexão com servidor na leitura

--- Usuario/src/ThreadUserLeitura.java
+++ Usuario/src/ThreadUserLeitura.java
@@ -1,20 +1,26 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ThreadUserLeitura extends Thread {
   private Socket socketCliente;
+  private BufferedReader doServidor;
 
-  public ThreadUserLeitura(Socket socketCliente) {
+  public ThreadUserLeitura(Socket socketCliente) throws IOException {
     this.socketCliente = socketCliente;
+    this.doServidor = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
   }
 
   public void run() {
     while (true) {
       try {
-        BufferedReader doServidor =
-            new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-        System.out.println(doServidor.readLine());
+        String mensagem = doServidor.readLine();
+        if (mensagem == null) {
+        	socketCliente.close();
+        	break;
+        }
+        System.out.println(mensagem);
       } catch (Exception e) {
         break;
       }
